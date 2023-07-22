51f17227158412e7d7844b92ae34755da3e16f9f
Alhamdu Bello <alhamdubello@hotmail.co.uk>
2023-07-22 05:50:55 +0100

    fix calc.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,13 +1,13 @@
 """
-Calculator  functions
+Calculator functions
 """
 
 
-def add(x,y):
-    """Add x and y and return results."""
+def add(x, y):
+    """Add x and y and return result."""
     return x + y
 
 
-def subtract(x,y):
-    """Subtract x from y and return results,"""
-    return x - y
+def subtract(x, y):
+    """Subtract x from y and return result,"""
+    return y - x
